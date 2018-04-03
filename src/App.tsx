473c18c1b75b8ecd2637e7e6bd8b1975d8adca5b
Micah Godbolt <mgodbolt@microsoft.com>
2018-04-03 16:18:28 -0700

    name change...again

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import './App.css';
 import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
-import { MyComponent } from './MyComponent';
+import { MyComponent } from './NewComponent';
 
 // Initialize Icons
 initializeIcons();
